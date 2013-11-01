891a91ecf0c5ce6c6246e08174ef60f53ab88866
Rainrider <dev@null.fake>
2013-11-01 03:50:14 +0100

    Add localization
    Need translations

--- core.lua
+++ core.lua
@@ -1,5 +1,6 @@
 local addonName, TimelessTreasures = ...
 TimelessTreasures.points = {}
+local L = TimelessTreasures.L
 
 -- our db and defaults
 local db
@@ -7,30 +8,32 @@ local defaults = { profile = { completed = false, icon_scale = 1.4, icon_alpha =
 
 local points = TimelessTreasures.points
 
-local moss = "Moss-Covered Chest"
-local sturdy = "Sturdy Chest"
-local smoldering = "Smoldering Chest"
-local skull = "Skull-Covered Chest"
-local blazing = "Blazing Chest"
+local moss = L["Moss-Covered Chest"]
+local sturdy = L["Sturdy Chest"]
+local smoldering = L["Smoldering Chest"]
+local skull = L["Skull-Covered Chest"]
+local blazing = L["Blazing Chest"]
 
 local default_icon = select(10, GetAchievementInfo(8729))
 
 -- http://www.wowhead.com/achievement=8729/treasure-treasure-everywhere
+-- mapFiles 5th or 1st return of GetMapInfo()
+-- coord 1st and 2nd return of GetPlayerMapPosition("player") times 10,000
+-- 0.12345678 and 0.87654321 -> [12348765]
 points["TimelessIsle"] = {
 	-- Moss-Covered Chests
-	-- [coordinates] = questID
 	[22204930] = {type = moss, quest = 33175},
 	[22246808] = {type = moss, quest = 33178},
 	[22423535] = {type = moss, quest = 33174},
-	[24623863] = {type = moss, quest = 33200, note = "On a treestump"},
+	[24623863] = {type = moss, quest = 33200, note = L["On a treestump"]},
 	[24755301] = {type = moss, quest = 33176},
 	[25522721] = {type = moss, quest = 33171},
 	[25664584] = {type = moss, quest = 33177},
-	[26016145] = {type = moss, quest = 33199, note = "On a treestump"},
+	[26016145] = {type = moss, quest = 33199, note = L["On a treestump"]},
 	[26856875] = {type = moss, quest = 33179},
 	[27363909] = {type = moss, quest = 33172},
-	[29683174] = {type = moss, quest = 33202, note = "On the ramp"},
-	[30603655] = {type = moss, quest = 33173, note = "On a treestump"},
+	[29683174] = {type = moss, quest = 33202, note = L["On the ramp"]},
+	[30603655] = {type = moss, quest = 33173, note = L["On a treestump"]},
 	[31007633] = {type = moss, quest = 33180},
 	[34858422] = {type = moss, quest = 33184},
 	[35367642] = {type = moss, quest = 33181},
@@ -38,31 +41,31 @@ points["TimelessIsle"] = {
 	[38737159] = {type = moss, quest = 33182},
 	[39797953] = {type = moss, quest = 33183},
 	[43568404] = {type = moss, quest = 33185},
-	[44136546] = {type = moss, quest = 33198, note = "On a treestump"},
+	[44136546] = {type = moss, quest = 33198, note = L["On a treestump"]},
 	[46764678] = {type = moss, quest = 33187},
 	[46955369] = {type = moss, quest = 33186},
 	[49716572] = {type = moss, quest = 33195},
-	[51174568] = {type = moss, quest = 33188, note = "In the slump scraps under the broken bridge"},
+	[51174568] = {type = moss, quest = 33188, note = L["In the slump scraps under the broken bridge"]},
 	[52756286] = {type = moss, quest = 33197},
 	[53097077] = {type = moss, quest = 33196},
 	[55524434] = {type = moss, quest = 33189},
 	[58015070] = {type = moss, quest = 33190},
 	[59913132] = {type = moss, quest = 33201},
 	[60176603] = {type = moss, quest = 33194},
-	[61648849] = {type = moss, quest = 33227, note = "In the shipwreck of Cpt. Zvezdan"},
+	[61648849] = {type = moss, quest = 33227, note = L["In the shipwreck of Cpt. Zvezdan"]},
 	[63815915] = {type = moss, quest = 33192},
 	[64917559] = {type = moss, quest = 33193},
 	[65634783] = {type = moss, quest = 33191},
 	-- Sturdy Chests
-	[26676495] = {type = sturdy, quest = 33205, note = "On the top of the cliff. Use the Highwind Albatross"},
-	[28193521] = {type = sturdy, quest = 33204, note = "On the top of the cliff. Use the Highwind Albatross"},
-	[59254946] = {type = sturdy, quest = 33207, note = "Inside the Mysterious Den. Use one of objects from Legends of the Past"},
+	[26676495] = {type = sturdy, quest = 33205, note = L["On the top of the cliff. Use the Highwind Albatross"]},
+	[28193521] = {type = sturdy, quest = 33204, note = L["On the top of the cliff. Use the Highwind Albatross"]},
+	[59254946] = {type = sturdy, quest = 33207, note = L["Inside the Mysterious Den. Use one of objects from Legends of the Past"]},
 	[64687047] = {type = sturdy, quest = 33206},
 	-- Smoldering Chests
 	[53987805] = {type = smoldering, quest = 33209},
 	[69573289] = {type = smoldering, quest = 33208},
 	-- Skull-Covered Chest
-	[46703230] = {type = skull, quest = 33203, note = "Inside Cavern of Lost Spirits (entrance at 43.2, 41.3)"},
+	[46703230] = {type = skull, quest = 33203, note = L["Inside Cavern of Lost Spirits (entrance at 43.2, 41.3)"]},
 	-- Blazing Chest
 	[47262682] = {type = blazing, quest = 33210},
 }
@@ -83,7 +86,7 @@ function TimelessTreasures:OnEnter(mapFile, coord)
 	end
 	local info = points[mapFile][coord]
 	tooltip:SetText(info.type)
-	tooltip:AddLine("Note: " .. (info.note or ""), 1, 1, 1, true)
+	tooltip:AddLine(L["Note: "] .. (info.note or ""), 1, 1, 1, true)
 	tooltip:Show()
 end
 
@@ -121,7 +124,7 @@ do
 		if level == 1 then
 			-- create the title of the menu
 			info.isTitle = 1
-			info.text = "HandyNotes - Timeless Treasures"
+			info.text = L.ADDON_NAME
 			info.notCheckable = 1
 
 			UIDropDownMenu_AddButton(info, level)
@@ -131,7 +134,7 @@ do
 				info.disabled = nil
 				info.isTitle = nil
 				info.notCheckable = nil
-				info.text = "Create waypoint"
+				info.text = L["Create waypoint"]
 				info.icon = nil
 				info.func = CreateWaypoint
 				info.arg1 = currentZone
@@ -144,7 +147,7 @@ do
 			info.disabled = nil
 			info.isTitle = nil
 			info.notCheckable = 1
-			info.text = "Close"
+			info.text = CLOSE
 			info.icon = nil
 			info.func = Close
 			info.arg1 = nil
@@ -193,8 +196,8 @@ end
 -- config
 local options = {
 	type = "group",
-	name = "Timeless Treasures",
-	desc = "Timeless Treasures locations.",
+	name = L.NAME,
+	desc = L["Timeless Treasures locations."],
 	get = function(info) return db[info[#info]] end,
 	set = function(info, v)
 		db[info[#info]] = v
@@ -202,13 +205,13 @@ local options = {
 	end,
 	args = {
 		desc = {
-			name = "These settings control the look and feel of the icon.",
+			name = L["These settings control the look and feel of the icon."],
 			type = "description",
 			order = 1,
 		},
 		completed = {
-			name = "Show completed",
-			desc = "Show icons for treasures you have already got.",
+			name = L["Show completed"],
+			desc = L["Show icons for treasures you have already got."],
 			type = "toggle",
 			width = "full",
 			arg = "completed",
@@ -216,16 +219,16 @@ local options = {
 		},
 		icon_scale = {
 			type = "range",
-			name = "Icon Scale",
-			desc = "Change the size of the icons.",
+			name = L["Icon Scale"],
+			desc = L["Change the size of the icons."],
 			min = 0.25, max = 2, step = 0.01,
 			arg = "icon_scale",
 			order = 3,
 		},
 		icon_alpha = {
 			type = "range",
-			name = "Icon Alpha",
-			desc = "Change the transparency of the icons.",
+			name = L["Icon Alpha"],
+			desc = L["Change the transparency of the icons."],
 			min = 0, max = 1, step = 0.01,
 			arg = "icon_alpha",
 			order = 4,
---
+++ localization.lua
@@ -0,0 +1,51 @@
+local addonName, ns = ...
+
+local default = {
+	ADDON_NAME = "HandyNotes - Timeless Treasures",
+	NAME = "Timeless Treasures",
+	-- chests
+	["Moss-Covered Chest"] = "Moss-Covered Chest",
+	["Sturdy Chest"] = "Sturdy Chest",
+	["Smoldering Chest"] = "Smoldering Chest",
+	["Skull-Covered Chest"] = "Skull-Covered Chest",
+	["Blazing Chest"] = "Blazing Chest",
+	-- notes
+	["Note: "] = "Note: ",
+	["On a treestump"] = "On a treestump",
+	["On the ramp"] = "On the ramp",
+	["In the slump scraps under the broken bridge"] = "In the slump scraps under the broken bridge",
+	["In the shipwreck of Cpt. Zvezdan"] = "In the shipwreck of Cpt. Zvezdan",
+	["On the top of the cliff. Use the Highwind Albatross"] = "On the top of the cliff. Use the Highwind Albatross",
+	["Inside the Mysterious Den. Use one of objects from Legends of the Past"] = "Inside the Mysterious Den. Use one of objects from Legends of the Past",
+	["Inside Cavern of Lost Spirits (entrance at 43.2, 41.3)"] = "Inside Cavern of Lost Spirits (entrance at 43.2, 41.3)",
+	-- buttons
+	["Create waypoint"] = "Create waypoint",
+	-- options
+	["Timeless Treasures locations."] = "Timeless Treasures locations.",
+	["These settings control the look and feel of the icon."] = "These settings control the look and feel of the icon.",
+	["Show completed"] = "Show completed",
+	["Show icons for treasures you have already got."] = "Show icons for treasures you have already got.",
+	["Icon Scale"] = "Icon Scale",
+	["Change the size of the icons."] = "Change the size of the icons.",
+	["Icon Alpha"] = "Icon Alpha",
+	["Change the transparency of the icons."] = "Change the transparency of the icons.",
+}
+
+local localization = {
+	["deDE"] = {
+		["Moss-Covered Chest"] = "Moosüberwucherte Truhe",
+		["Sturdy Chest"] = "Robuste Truhe",
+		["Smoldering Chest"] = "Schwelende Truhe",
+		["Skull-Covered Chest"] = "Totenkopftruhe",
+		["Blazing Chest"] = "Lodernde Truhe",
+	},
+}
+
+do
+	for locale, tbl in pairs(localization) do
+		locale = setmetatable(tbl, {__index = default})
+	end
+end
+
+
+ns.L = localization[GetLocale()] or default
